f7ae2e7141795b6faed037e4281a562fa2068579
huangyongsheng <huangyongsheng@kkworld.com>
2019-08-22 18:19:16 +0800

    first commit

---
+++ src/main/java/VectorDistanceConfig.java
@@ -0,0 +1,8 @@
+/**
+ * @author yshhuang@foxmail.com
+ * @date 2019-08-22 15:34
+ */
+public class VectorDistanceConfig {
+    public static final String ENGINE_TYPE = "vector-distance";
+    public static final String SCRIPT_SOURCE="yshhuang";
+}
